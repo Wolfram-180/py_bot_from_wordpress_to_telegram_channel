251d02a827a3bca3712fca13dd60bc48f28ca6da
Wolfram-180 <sergei.ulvis@gmail.com>
2022-04-07 16:58:56 +0300

    moving on

--- tlgrm_bot_sendimg_files_to_channel.py
+++ tlgrm_bot_sendimg_files_to_channel.py
@@ -111,7 +111,7 @@ async def send_files(message: types.Message):
 
         await logandmess('Sent: ' + full_path_to_send, message.chat.id)
 
-        for sknd in range(10, 1):
+        for sknd in range(1, 10):
             await bot.send_message(message.chat.id, sknd) 
             sleep(1)
 
