490e2ffb4f2c539bfe165327c1e0f1fc8831c2fa
Wolfram-180 <sergei.ulvis@gmail.com>
2022-04-07 15:49:03 +0300

    all done and works fine

--- tlgrm_bot_sendimg_files_to_channel.py
+++ tlgrm_bot_sendimg_files_to_channel.py
@@ -1,4 +1,5 @@
 import logging
+from time import sleep
 from tkinter import NO
 from aiogram import executor
 from aiogram import Bot, Dispatcher, types
@@ -11,7 +12,7 @@ import os
 
 import environment_params
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(filename='botlog.log', encoding='utf-8', level=logging.INFO)
 
 API_TOKEN = environment_params.admin_bot_token
 
@@ -71,14 +72,29 @@ async def list_files(message: types.Message):
 
 @dp.message_handler(commands=[cmnd_send_files_to_channel])
 async def send_files(message: types.Message):
-    #file_list = '; '.join(os.listdir(environment_params.load_path))
-    first_file_name = os.listdir(environment_params.load_path)[0]
-    #await bot.send_message(environment_params.chnl_ID, 'Implementation in process:' + first_file_name, parse_mode = 'Markdown')  
-    await bot.send_message(
-        environment_params.chnl_ID,
-        md.text(first_file_name),
-        reply_markup=None,
-    )  
+    #first_file_name = os.listdir(environment_params.load_path)[0]
+
+    file_list = os.listdir(environment_params.load_path)
+
+    for fl in file_list:
+        file_name, file_extension = os.path.splitext(fl)
+        full_path_to_send = environment_params.load_path+fl
+        if file_extension == '.gif':
+            await bot.send_animation(chat_id=environment_params.chnl_ID, 
+                animation=open(full_path_to_send, 'rb'))
+        elif ((file_extension == '.mp4') or (file_extension == '.webm')):
+            await bot.send_video(chat_id=environment_params.chnl_ID, 
+                video=open(full_path_to_send, 'rb'))
+        elif ((file_extension == '.jpg') 
+            or (file_extension == '.jpeg')
+            or (file_extension == '.png')):
+            await bot.send_photo(chat_id=environment_params.chnl_ID, 
+                photo=open(full_path_to_send, 'rb'))                
+        else:
+            await bot.send_document(chat_id=environment_params.chnl_ID, 
+                document=open(full_path_to_send, 'rb'))
+        logging.info('Sent: ' + full_path_to_send)
+        sleep(1)
 
 
 if __name__ == '__main__':
